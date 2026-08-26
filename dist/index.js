0d893b80959f93297e6ef6db7d3ef9ba5c2ae23b
Tomás Maritano <tomymaritano@gmail.com>
2026-08-25 18:55:02 -0700

    feat: add theme.json and pack it in v0.1.1 tarball

--- dist/index.js
+++ dist/index.js
@@ -35,7 +35,7 @@ const TOKENS = {
 module.exports = {
   id: 'theme-parchment',
   name: 'Parchment',
-  version: '0.1.0',
+  version: '0.1.1',
   description: 'Warm paper palette. Official Dripnex theme.',
 
   activate(context) {
